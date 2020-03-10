6758f0388e50e2c6ff3acf6afbc92a0a283b8d98
Doyosae <calidris.snipe@gmail.com>
2020-03-11 02:08:32 +0900

    add example 3

---
+++ 03_Enhancement_Example/dataload.py
@@ -0,0 +1,164 @@
+import os, librosa
+import numpy as np
+
+
+class dataLoader():
+    
+    def __init__ (self, sr = 16000, sliceCut1 = 744, sliceCut2 = 930):
+
+        self.sr = sr
+        self.sliceCut1 = sliceCut1
+        self.sliceCut2 = sliceCut2
+        
+    def createLabel (self):
+        """
+        라벨에 사용할 음성을 불러와서 라벨 데이터셋을 만드는 코드
+        노이즈 음성이 한 음성 당 31개의 노이즈여서, 해당 라벨도 각각 31개씩 맞춤
+        1 : label[0] ~ label[30], 31개
+        2 : label[31] ~ label[61], 31개
+        3 : label[62] ~ label[92], 31개
+        ... ... 해서 30번 음성까지 있음 (index range is from 0 to 929
+        """
+        labelList, trainLabel, testLabel = [], [], []
+        
+        # 깨끗한 음성 파일을 모두 풀러오면 labelList은 30개의 경로를 담고 
+        for root, dirs, files in os.walk('./clean'):
+            for fname in files:
+                full_fname = os.path.join(root, fname)
+                labelList.append(full_fname)
+        
+        # len(LabelList) == 30
+        for index in range (len(labelList)):
+            rawSound, sr = librosa.load(labelList[index], sr = self.sr)
+
+            for repeat in range (24):
+                trainLabel.append(rawSound)
+            
+            for repeat in range (7):
+                testLabel.append(rawSound)
+                
+        return trainLabel, testLabel
+
+
+    def createTrain (self):
+        """
+        훈련에 사용할 mixture 데이터 720개
+        1번 음성에 대하여 24가지 노이즈가 껴있음 (0dB, 5dB, 10dB 마다)
+        2번 음성도 마찬가지로 31가지 노이즈가 껴있음
+        이렇게 30번까지 음원을 고려하면 720개의 훈련셋
+        """
+        dataList, trainSound = [], []
+        
+        for root, dirs, files in os.walk('./train'):
+            for fname in files:
+                full_fname = os.path.join(root, fname)
+                dataList.append(full_fname)
+                
+        # len(dataList) == 720
+        for index in range (len(dataList)):
+            noiseSound, sr = librosa.load(dataList[index], sr = self.sr)
+            trainSound.append(noiseSound)
+            
+        return trainSound
+    
+    
+    def createTest (self):
+        """
+        테스트에 사용할 데이터 210개
+        """
+        dataList, testSound = [], []
+        
+        for root, dirs, files in os.walk('./test'):
+            for fname in files:
+                full_fname = os.path.join(root, fname)
+                dataList.append(full_fname)
+        
+        # len(dataList) == 210
+        for index in range (len(dataList)):
+            noiseSound, sr = librosa.load(dataList[index], sr = self.sr)
+            testSound.append(noiseSound)
+            
+        return testSound
+        
+
+    def callData (self):
+        """
+        훈련용 라벨, 테스트용 라벨, 훈련용 데이터, 테스트용 데이터의 크기는
+        (930,) (930,) (45058,) (45058,)
+        """
+        trainLabel , testLabel = [], []
+        trainData, testData = [], []
+        
+        trainLabel, testLabel = self.createLabel()
+        trainData = self.createTrain()
+        testData = self.createTest()
+
+        # size is (744,) (186,) (744,) (186,), repectively
+        print("훈련 라벨의 크기   : ", np.shape(trainLabel))
+        print("검증 라벨의 크기   : ", np.shape(testLabel))
+        print("훈련 데이터의 크기 : ", np.shape(trainData))
+        print("검증 데이터의 크기 : ", np.shape(testData))
+        
+        print("\n")
+        print("각 데이터마다의 시퀀스 길이 (음성 길이) \n")
+        print("Length of trainLabel sequence : ", np.shape(trainLabel[0]))
+        print("Length of testLabel sequence  : ", np.shape(testLabel[0]))
+        print("Length of trainData sequence  : ", np.shape(trainData[0]))
+        print("Length of testData sequence   : ", np.shape(testData[0]))
+        
+        return trainLabel, testLabel, trainData, testData
+    
+
+class fixedLength (dataLoader):
+    """
+    이 클래스를 만든 목적.
+    1. 위의 dataLoader 클래스는 소리의 시퀀스가 가변적이다.
+    2. 가변적인 시퀀스는 RNN, LSTM에 대응하면 되지만, CNN 등에 대응하기는 힘듬
+    3. 따라서 고정 길이의 시퀀스를 만들기 위하여 이 클래스가 필요할 것.
+    """
+
+    
+    def slicingData (self):
+        
+        trainLabel, testLabel, trainData, testData = super().callData()
+        
+        sliceLength = []
+        
+        # Find minimus array element size , for slicing all array
+        for index in range (len(trainLabel)):
+            sliceLength.append(len(trainLabel[index]))
+            
+        minLenght = min(sliceLength)
+        for index in range (len(trainLabel)):
+            trainLabel[index] = trainLabel[index][:minLenght]
+            trainData[index] = trainData[index][:minLenght]
+            
+        for index in range (len(testLabel)):
+            testLabel[index] = testLabel[index][:minLenght]
+            testData[index] = testData[index][:minLenght]
+            
+        return trainLabel, trainData, testLabel, testData
+    
+    
+    def callData (self):
+        """
+        훈련용 라벨, 테스트용 라벨, 훈련용 데이터, 테스트용 데이터의 크기는
+        (930,),  (930,), (45058,) (45058,)
+        """
+        trainLabel , testLabel = [], []
+        trainData, testData = [], []
+        
+        trainLabel, trainData, testLabel, testData = self.slicingData()
+
+        # size is (744,) (186,) (744,) (186,), repectively
+        print("분리한 훈련 라벨의 크기   : ", np.shape(trainLabel))
+        print("분리한 훈련 데이터의 크기   : ", np.shape(trainData))
+        print("분리한 검증 라벨의 크기 : ", np.shape(testLabel))
+        print("분리한 검증 데이터의 크기 : ", np.shape(testData))
+
+        return trainLabel, testLabel, trainData, testData
+    
+    
+if __name__ == '__main__':
+    data = fixedLength()
+    data.callData()
---
+++ 03_Enhancement_Example/test.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+from train import *
+from utility import *
+from dataload import *
+from processing import *
+
+test = enhancement()
+model = test.model()
+
+# relu_50_BCE or relu_50_MSE
+model.load_weights("./save/relu_50_BCE.h5")
+
+if __name__ == '__main__':
+
+    # trianLabel, testLabel, trainSound, testSound
+    trainLabel, testLabel, trainSound, testSound = test.get_data()
+
+    # Exploit estimated mask
+    estimated_train_mask = model.predict(x = trainSound, batch_size = 60 , verbose = 1)
+    estimated_test_mask  = model.predict(x = testSound, batch_size = 60, verbose = 1)
+
+    # Calculate estimated speech
+    estimated_train_Speech = np.multiply(trainSound, estimated_train_mask)
+    estimated_test_Speech  = np.multiply(testSound, estimated_test_mask)
+    
+    # show Spectogram about train sound -> estimated train speech and
+    # test sound (15db) -> estimated test speech
+    showSpectogram(trainSound[0], "train sound")
+    showSpectogram(estimated_train_Speech[0], "estimated train speech")
+    showSpectogram(testSound[0], "test sound")
+    showSpectogram(estimated_test_Speech[0], "estimated test speech")
+    
+    
+#     # 음원 파일로 저장하기 위하여 채널 1을 없애고, isftft 처리한 후, 저장
+#     estimated_train_Speech = np.reshape(estimated_train_Speech, (720, 256, 256))
+#     estimated_test_Speech  = np.reshape(estimated_test_Speech, (210, 256, 256))
+#     train = librosa.core.istft(estimated_train_Speech[0])
+#     test  = librosa.core.istft(estimated_test_Speech[0])
+#     librosa.output.write_wav("./result/estimated_train.wav", train, sr = 16000, norm=False)
+#     librosa.output.write_wav("./result/estimated_test.wav", test, sr = 16000, norm=False)
+
